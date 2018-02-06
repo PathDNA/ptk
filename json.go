66e3258b60670416b7bd83099fe3676bfdbd9ea7
OneOfOne <oneofone@gmail.com>
2018-02-06 23:21:45 +0200

    Base64ToJSON

--- json.go
+++ json.go
@@ -2,6 +2,7 @@ package ptk
 
 import (
 	"bufio"
+	"encoding/base64"
 	"encoding/json"
 	"io"
 
@@ -12,28 +13,38 @@ type M = apiserv.M
 
 // PipeJSONObject uses an io.Pipe to stream an M rather than keeping it in memory.
 // if the type of value is io.Reader, it will be directly streamed (so it must be valid JSON).
+// if the type is func(io.Writer|*bufio.Writer) error, it will pass the underlying writer.
 // otherwise json.Marshal will be called on the value.
 func PipeJSONObject(obj M) io.Reader {
 	pr, pw := io.Pipe()
 
 	go func() {
 		bw := bufio.NewWriter(pw)
-		if err := pipeJSONObject(bw, obj); err != nil {
+		if err := WriteJSONObject(bw, obj); err != nil {
 			pw.CloseWithError(err)
 		} else {
 			pw.CloseWithError(bw.Flush())
 		}
-		pr.Close()
+		// pr.Close()
 
 	}()
 	return pr
 }
 
-func pipeJSONObject(bw *bufio.Writer, obj M) (err error) {
+func WriteJSONObject(w io.Writer, obj M) (err error) {
 	var (
 		b  []byte
 		ln = len(obj)
+		bw *bufio.Writer
 	)
+
+	if ww, ok := w.(*bufio.Writer); ok {
+		bw = ww
+	} else {
+		bw = bufio.NewWriter(w)
+		defer bw.Flush()
+	}
+
 	bw.WriteByte('{')
 	for k, v := range obj {
 		ln--
@@ -48,11 +59,19 @@ func pipeJSONObject(bw *bufio.Writer, obj M) (err error) {
 				return
 			}
 		case M:
-			if err = pipeJSONObject(bw, v); err != nil {
+			if err = WriteJSONObject(bw, v); err != nil {
 				return
 			}
 		case map[string]interface{}:
-			if err = pipeJSONObject(bw, v); err != nil {
+			if err = WriteJSONObject(bw, v); err != nil {
+				return
+			}
+		case func(w io.Writer) error:
+			if err = v(bw); err != nil {
+				return
+			}
+		case func(w *bufio.Writer) error:
+			if err = v(bw); err != nil {
 				return
 			}
 		default:
@@ -69,4 +88,30 @@ func pipeJSONObject(bw *bufio.Writer, obj M) (err error) {
 	bw.WriteByte('}')
 
 	return
+}
+
+// Base64ToJSON allows converting a reader into a base64 encoded string and streaming it directly without using buffers.
+// if enc is nil, it uses base64.StdEncoding by default.
+// Example: WriteJSONObject(w, M{"image": Base64ToJSON(nil, "image/png", r)})
+func Base64ToJSON(enc *base64.Encoding, mimeType string, r io.Reader) func(w io.Writer) error {
+	if enc == nil {
+		enc = base64.StdEncoding
+	}
+
+	return func(w io.Writer) error {
+		var bw *bufio.Writer
+		if ww, ok := w.(*bufio.Writer); ok {
+			bw = ww
+		} else {
+			bw = bufio.NewWriter(w)
+			defer bw.Flush()
+		}
+
+		bw.WriteString(`"data:` + mimeType + `;base64,`)
+		enc := base64.NewEncoder(enc, bw)
+		_, err := io.Copy(enc, r)
+		enc.Close()
+		bw.WriteByte('"')
+		return err
+	}
 }
