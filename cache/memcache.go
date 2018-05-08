996d45f7ab83559f21063b140572a8c605dacdc6
OneOfOne <oneofone@gmail.com>
2018-05-08 02:28:22 +0200

    use double mutex to handle large updates

--- cache/memcache.go
+++ cache/memcache.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"sync"
 	"time"
+
+	"github.com/PathDNA/atoms"
 )
 
 func NewMemCache(autoCleanEvery time.Duration) (mc *MemCache) {
@@ -35,74 +37,93 @@ func NewMemCache(autoCleanEvery time.Duration) (mc *MemCache) {
 type MemCache struct {
 	c    map[string]*cacheItem
 	done chan struct{}
-	m    sync.RWMutex
+	// using double mutexes to handle long updates
+	mux  sync.RWMutex
+	mmux atoms.MultiMux
 }
 
-func (mc *MemCache) Set(key string, val interface{}, ttl time.Duration) error {
-	mc.m.Lock()
-	defer mc.m.Unlock()
-
-	if mc.c == nil {
-		return os.ErrClosed
-	}
-	mc.c[key] = &cacheItem{val, time.Now().Add(ttl).Unix()}
-
-	return nil
-}
-
-func (mc *MemCache) Delete(key string) error {
-	mc.m.Lock()
-	defer mc.m.Unlock()
-
-	if mc.c == nil {
-		return os.ErrClosed
-	}
-	delete(mc.c, key)
-
-	return nil
-}
-
-func (mc *MemCache) Get(key string) (val interface{}, found bool) {
-	var ci *cacheItem
-	mc.m.RLock()
-	if ci, found = mc.c[key]; found {
-		val = ci.Value
-	}
-	mc.m.RUnlock()
+func (mc *MemCache) Set(key string, val interface{}, ttl time.Duration) (err error) {
+	mc.mmux.Update(key, func() {
+		mc.mux.Lock()
+		if mc.c == nil {
+			err = os.ErrClosed
+		} else {
+			mc.c[key] = &cacheItem{val, time.Now().Add(ttl).Unix()}
+		}
+		mc.mux.Unlock()
+	})
 
 	return
 }
 
-func (mc *MemCache) GetAndDelete(key string) (val interface{}, found bool) {
-	var ci *cacheItem
-	mc.m.Lock()
-	if ci, found = mc.c[key]; found {
-		val = ci.Value
-		delete(mc.c, key)
-	}
-	mc.m.Unlock()
+func (mc *MemCache) Update(key string, fn func(old interface{}) (val interface{}, ttl time.Duration)) (err error) {
+	mc.mmux.Update(key, func() {
+		var old interface{}
+		mc.mux.RLock()
+		if ci := mc.c[key]; ci != nil {
+			old = ci.Value
+		}
+		mc.mux.RUnlock()
+		val, ttl := fn(old)
+
+		mc.mux.Lock()
+		if mc.c == nil {
+			err = os.ErrClosed
+		} else {
+			mc.c[key] = &cacheItem{val, time.Now().Add(ttl).Unix()}
+		}
+		mc.mux.Unlock()
+	})
+
+	return
+}
+
+func (mc *MemCache) Delete(key string) (err error) {
+	mc.mmux.Update(key, func() {
+		mc.mux.Lock()
+		if mc.c == nil {
+			err = os.ErrClosed
+		} else {
+			delete(mc.c, key)
+		}
+		mc.mux.Unlock()
+	})
+	mc.mmux.Delete(key)
+	return
+}
+
+func (mc *MemCache) Get(key string) (val interface{}, found bool) {
+	mc.mmux.Read(key, func() {
+		var ci *cacheItem
+		mc.mux.RLock()
+		if ci, found = mc.c[key]; found {
+			val = ci.Value
+		}
+		mc.mux.RUnlock()
+	})
 
 	return
 }
 
 func (mc *MemCache) Clean() (n int) {
 	now := time.Now().Unix()
-	mc.m.Lock()
+	mc.mux.Lock()
 	for k, ci := range mc.c {
 		if now > ci.ExpiresAt {
 			delete(mc.c, k)
+			go mc.mmux.Delete(k)
 			n++
 		}
 	}
-	mc.m.Unlock()
+	mc.mux.Unlock()
 
 	return
 }
 
 func (mc *MemCache) Reset() {
-	mc.m.Lock()
+	mc.mux.Lock()
 	mc.c = map[string]*cacheItem{}
-	mc.m.Unlock()
+	mc.mux.Unlock()
 }
 
 func (mc *MemCache) Close() error {
@@ -112,10 +133,10 @@ func (mc *MemCache) Close() error {
 	default:
 	}
 
-	mc.m.Lock()
+	mc.mux.Lock()
 	close(mc.done)
 	mc.c = nil
-	mc.m.Unlock()
+	mc.mux.Unlock()
 
 	return nil
 }
